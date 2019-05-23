321ac93b90b0f51832f9e19d45f042fe6d71088e
Orel Karavani <orel1192@gmail.com>
2019-05-23 18:21:09 +0300

    BooksList style

--- client/src/BooksList/BooksList.js
+++ client/src/BooksList/BooksList.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react'
 import BookCard from "../BookCard/BookCard";
+import styled from "styled-components";
 
+const BooksListStyle = styled.div`
+    display: flex
+    flex-direction: row
+`;
 
 class BooksList extends Component {
 
@@ -12,14 +17,14 @@ class BooksList extends Component {
         console.log("books list");
         console.log(this.props.books);
         return (
-            <div>
+            <BooksListStyle>
                 {
                     this.props.books.map(item => (
                             <BookCard key={new Date()} book={item}/>
                         )
                     )
                 }
-            </div>
+            </BooksListStyle>
         );
     }
 }
