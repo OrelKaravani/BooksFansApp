8addcbd75b9af254eeac79c3a08274044b7bd5f7
unknown <itay010197@gmail.com>
2019-05-23 16:05:12 +0300

    adding data

---
+++ routes/books.js
@@ -0,0 +1,47 @@
+var express = require('express')
+const fs = require('fs')
+const path = require('path')
+var router = express.Router()
+
+router.get('/', function (req, res) {
+    res.json(getAllBooks());
+})
+
+router.get('/book/:book', function (req, res) {
+    res.json(getBook(req.params["book"]))
+})
+
+router.get('/author/:author', function (req, res) {
+    res.json(getAuthorBooks(req.params["author"]))
+})
+
+function getAllBooks(){
+    let rawdata = fs.readFileSync(path.join(__dirname, 'books.json'));  
+    let books = JSON.parse(rawdata);
+    return books;
+}
+
+function getBook(book){
+    let books = getAllBooks();
+
+    for(let i =0; i < books.length; i++){
+        if(book == books[i]["title"])
+        return books[i];
+    }
+
+    return {}
+}
+
+function getAuthorBooks(author){
+    let books = [];
+    let allBooks = getAllBooks();
+
+    for(let i =0; i < allBooks.length; i++){
+        if(author == allBooks[i]["author"])
+            books.push(allBooks[i]);
+    }
+
+    return books;
+}
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
+const books = require('./routes/books')
 
 const app = express();
 const port = process.env.PORT || 80;
 
 // Serve any static files built by React
 app.use(express.static(path.join(__dirname, "client/build")));
+app.use('/books', books);
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
